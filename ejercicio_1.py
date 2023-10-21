2ac9afd73f1e8e3f0fd27ed078286ce6b8324468
silvina <svilchez@fi.uba.ar>
2023-10-21 15:53:05 -0300

    se agrega ejericio

---
+++ ejercicio_1.py
@@ -0,0 +1,11 @@
+"""El cifrado de César sustituye cada letra del mensaje por otra. La letra sustituida se obtiene
+“desplazando” cada letra del abecedario una cierta cantidad de posiciones a la derecha. Esta
+cantidad de lugares que una letra se desplaza es la clave secreta que se debe pasar al receptor del
+mensaje para poder descifrarlo.
+Consideraciones
+● Se deben considerar tanto las letras mayúsculas como minúsculas.
+● Adapte el método para encriptar de forma similar los dígitos numéricos que pueda tener el
+mensaje.
+● Los espacios y otros símbolos no se codifican, quedan igual.
+● Sugerimos que lean sobre las funciones ord() y chr(). Cada letra tiene asignado un código
+numérico (llamado código ASCII) y letras consecutivas tienen números consecutivos."""
