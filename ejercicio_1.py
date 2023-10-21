78cef7122a453622bcc1c7f3d6caef6b7af70574
silvina <svilchez@fi.uba.ar>
2023-10-21 19:18:33 -0300

    Completando el ejercicio

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -9,3 +9,45 @@ mensaje.
 ● Los espacios y otros símbolos no se codifican, quedan igual.
 ● Sugerimos que lean sobre las funciones ord() y chr(). Cada letra tiene asignado un código
 numérico (llamado código ASCII) y letras consecutivas tienen números consecutivos."""
+
+def codigo_cesar(mensaje, clave) :
+    """
+    >>> codigo_cesar ("%&2345Pm ", 3)
+    '%&5678Sp '
+    >>> codigo_cesar ("EJERCICIO", 3)
+    'HMHUFLFLR'
+    >>> codigo_cesar ("ejercicio", 2)
+    'glgtekekq'
+    >>> codigo_cesar ("1 23456789", 4)
+    '5 67890123'
+    
+    """
+    cadena_cifrada = ""
+    for caracter in mensaje :
+        if caracter.islower() :
+            if ord(caracter) + clave > ord("z") :
+                cadena_cifrada += chr((ord(caracter) + clave) - 26) 
+            else:
+                cadena_cifrada += chr(ord(caracter) + clave)
+        if caracter.isupper() :
+            if ord(caracter) + clave > ord("Z") :
+                cadena_cifrada += chr((ord(caracter) + clave) - 26)
+            else:
+                cadena_cifrada += chr(ord(caracter) + clave)  
+        if caracter.isnumeric() :
+            if ord(caracter) + clave > ord("9") :
+                cadena_cifrada += chr((ord(caracter) + clave) - 10)
+            else:
+                cadena_cifrada += chr(ord(caracter) + clave)
+            
+        if 32 <= ord(caracter) <= 47 :
+            cadena_cifrada += chr(ord(caracter))
+            
+             
+                
+    return cadena_cifrada
+import doctest
+print(doctest.testmod())            
+            
+            
+    
